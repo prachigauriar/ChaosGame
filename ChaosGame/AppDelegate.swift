a48f8a7a011f0ab3d72a49738f3d4f1d819d6794
Prachi Gauriar <prachi@quantumlenscap.com>
2017-04-29 14:49:30 -0400

    Initial Commit

---
+++ ChaosGame/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ChaosGame
+//
+//  Created by Prachi Gauriar on 4/29/2017.
+//  Copyright © 2017 Prachi Gauriar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
